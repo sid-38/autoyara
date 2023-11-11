5ae22352897b9650e1d65f38cc1f9c45852a06ed
Soumyajyoti Dutta <soumyajejyoti1998@gmail.com>
2023-11-11 17:15:54 -0600

    ngrams

---
+++ ngrams.py
@@ -0,0 +1,27 @@
+from collections import Counter
+
+def extract_byte_ngrams(file_path, n):
+    with open(file_path, 'rb') as file:
+        # Read the binary content of the file
+        binary_content = file.read()
+
+        # Extract byte n-grams
+        byte_ngrams = [binary_content[i:i+n] for i in range(len(binary_content) - n + 1)]
+
+    return byte_ngrams
+
+def frequency(byte_ngrams):
+
+	return Counter(byte_ngrams)
+
+# Example usage: Extracting byte 3-grams from an exe file
+file_path = '/Users/soumyajyotidutta/Desktop/autoyara/calc.exe'
+n = 3
+result = extract_byte_ngrams(file_path, n)
+frequency = frequency(result)
+
+# Print the result
+for ngram in result:
+    print(ngram)
+
+print(frequency)
